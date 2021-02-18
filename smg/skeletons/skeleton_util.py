2ef6b52c78f7358fdd28d5782d471b603c0b421b
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-18 14:20:09 +0000

    Start trying to implement depth image depopulation.

--- smg/skeletons/skeleton_util.py
+++ smg/skeletons/skeleton_util.py
@@ -1,4 +1,9 @@
+import cv2
 import numpy as np
+
+from typing import List, Tuple
+
+from smg.utility import GeometryUtil
 
 from .skeleton import Skeleton
 
@@ -7,6 +12,24 @@ class SkeletonUtil:
     """Utility functions related to skeletons."""
 
     # PUBLIC STATIC METHODS
+
+    @staticmethod
+    def depopulate_depth_image(skeletons: List[Skeleton], depth_image: np.ndarray, world_from_camera: np.ndarray,
+                               intrinsics: Tuple[float, float, float, float], *, debug: bool = False) -> np.ndarray:
+        ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
+            depth_image, world_from_camera, intrinsics
+        )
+
+        depopulated_depth_image: np.ndarray = depth_image.copy()
+
+        for skeleton in skeletons:
+            person_mask: np.ndarray = SkeletonUtil.make_person_mask(skeleton, ws_points)
+            depopulated_depth_image = np.where(person_mask == 0, depopulated_depth_image, 0.0)
+
+            if debug:
+                cv2.imshow("Person Mask", person_mask)
+
+        return depopulated_depth_image
 
     @staticmethod
     def make_person_mask(skeleton: Skeleton, ws_points: np.ndarray) -> np.ndarray:
@@ -23,6 +46,10 @@ class SkeletonUtil:
         max_x += 0.3
         max_y += 0.3
         max_z += 0.3
+
+        # cx, cy, cz = (min_x + max_x) / 2, (min_y + max_y) / 2, (min_z + max_z) / 2
+        # min_x, min_y, min_z = min(min_x - 0.2, cx - 0.3), min(min_y - 0.2, cy - 0.3), min(min_z - 0.2, cz - 0.3)
+        # max_x, max_y, max_z = max(max_x + 0.2, cx + 0.3), max(max_y + 0.2, cy + 0.3), max(max_z + 0.2, cz + 0.3)
 
         xs, ys, zs = ws_points[:, :, 0], ws_points[:, :, 1], ws_points[:, :, 2]
 
