e038aa37dee9f286d4b395261957ba0f4562deb7
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-11 12:47:11 +0100

    Fix some typing issues.

--- smg/skeletons/skeleton_evaluator.py
+++ smg/skeletons/skeleton_evaluator.py
@@ -12,9 +12,9 @@ class SkeletonEvaluator:
     # CONSTRUCTOR
 
     def __init__(self, relevant_keypoints: List[str]):
-        self.__keypoint_to_index_map: Dict[str, int] = dict(
+        self.__keypoint_to_index_map = dict(
             zip(relevant_keypoints, np.arange(len(relevant_keypoints)))
-        )
+        )  # type: Dict[str, int]
 
     # PUBLIC STATIC METHODS
 
@@ -32,32 +32,36 @@ class SkeletonEvaluator:
     # PUBLIC METHODS
 
     def calculate_3d_pcks(self, correct_keypoint_table: np.ndarray) -> Dict[str, float]:
-        pcks: Dict[str, float] = dict()
-        row_count: int = correct_keypoint_table.shape[0]
+        pcks = dict()                                # type: Dict[str, float]
+        row_count = correct_keypoint_table.shape[0]  # type: int
+
         for keypoint_name, keypoint_index in self.__keypoint_to_index_map.items():
             pcks[keypoint_name] = 100 * np.sum(correct_keypoint_table[:, keypoint_index]).item() / row_count
+
         return pcks
 
     def calculate_mpjpes(self, per_joint_error_table: np.ndarray) -> Dict[str, float]:
-        mpjpes: Dict[str, float] = dict()
+        mpjpes = dict()  # type: Dict[str, float]
+
         for keypoint_name, keypoint_index in self.__keypoint_to_index_map.items():
             mpjpes[keypoint_name] = np.nanmean(per_joint_error_table[:, keypoint_index]).item()
+
         return mpjpes
 
     def make_per_joint_error_table(self, matched_skeletons: List[List[Tuple[Skeleton3D, Optional[Skeleton3D]]]]) \
             -> np.ndarray:
-        rows: List[np.ndarray] = []
+        rows = []  # type: List[np.ndarray]
 
         for frame in matched_skeletons:
-            row: np.ndarray = np.full(len(self.__keypoint_to_index_map), np.nan)
+            row = np.full(len(self.__keypoint_to_index_map), np.nan)  # type: np.ndarray
 
             for gt_skeleton, detected_skeleton in frame:
                 if detected_skeleton is None:
                     continue
 
                 for keypoint_name, gt_keypoint in gt_skeleton.keypoints.items():
-                    keypoint_index: Optional[int] = self.__keypoint_to_index_map.get(keypoint_name)
-                    detected_keypoint: Optional[Keypoint] = detected_skeleton.keypoints.get(keypoint_name)
+                    keypoint_index = self.__keypoint_to_index_map.get(keypoint_name)    # type: Optional[int]
+                    detected_keypoint = detected_skeleton.keypoints.get(keypoint_name)  # type: Optional[Keypoint]
                     if keypoint_index is not None and detected_keypoint is not None:
                         row[keypoint_index] = np.linalg.norm(detected_keypoint.position - gt_keypoint.position)
 
